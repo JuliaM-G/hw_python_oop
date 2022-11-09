2a1f1dcfec2951d003b0ff5235fc93b9a1e74a55
Юлия Мелик-Гусейнова <yu.melik-guseinova@yandex.ru>
2022-11-09 22:56:11 +0300

    исправленный код

--- homework.py
+++ homework.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass  # add dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
@@ -14,11 +18,11 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return f'Тип тренировки: {self.training_type}; ' \
-               f'Длительность: {self.duration:.3f} ч.; ' \
-               f'Дистанция: {self.distance:.3f} км; ' \
-               f'Ср. скорость: {self.speed:.3f} км/ч; ' \
-               f'Потрачено ккал: {self.calories:.3f}.'
+        return (f'Тип тренировки: {self.training_type}; '
+                f'Длительность: {self.duration:.3f} ч.; '
+                f'Дистанция: {self.distance:.3f} км; '
+                f'Ср. скорость: {self.speed:.3f} км/ч; '
+                f'Потрачено ккал: {self.calories:.3f}.')  # Добавление скобок
 
 
 class Training:
@@ -52,7 +56,7 @@ class Training:
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
         return InfoMessage(
-            type(self).__name__,
+            self.__class__.__name__,  # Вместо type(self).__name__
             self.duration,
             self.get_distance(),
             self.get_mean_speed(),
@@ -62,14 +66,7 @@ class Training:
 class Running(Training):
     """Тренировка: бег."""
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
-    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
-
-    def __init__(self,
-                 action,
-                 duration,
-                 weight
-                 ):
-        super().__init__(action, duration, weight)
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.79  # __init__ убран
 
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
@@ -136,7 +133,9 @@ def read_package(workout_type: str, data: list) -> Training:
         'RUN': Running,
         'WLK': SportsWalking
     }
-    return (training_types[workout_type](*data))
+    if workout_type not in training_types.keys():
+        raise KeyError('Нет такого типа тренировки')  # добавлен  error message
+    return training_types[workout_type](*data)  # скобки убраны
 
 
 def main(training: Training) -> None:
